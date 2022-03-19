a747e3b1c2ba5bf070f52ceba40c8a63df629802
apodacaj1995 <contactjackiecodes@gmail.com>
2022-03-19 16:00:48 -0600

    updated js
    fill big cup; changed measurement to oz in html

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
+updateBigCup()
+
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx))
 })
 
 function highlightCups(idx){
 
+    // toggle the last cup fill
     if(smallCups[idx].classList.contains('full') && !smallCups
     [idx].nextElementSibling.classList.contains('full')) {
         idx--
@@ -21,4 +24,34 @@ function highlightCups(idx){
             cup.classList.remove('full')
         }
     })
+
+    updateBigCup()
 }
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+
+    // nothing in cup? NO percentage
+    if(fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } 
+    // cup is filled? SHOW percentage
+    else {
+        percentage.style.visibility = 'visible'
+        //this shows fill in big cup of percentage
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        //this shows numerical percentage
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+
+    if (fullCups == totalCups){
+        //completely full cup? don't show remained
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    }else{
+        remained.style.visibility = 'visible'
+        liters.innerText = `${64 - (8 * fullCups)}`
+        }
+    }
